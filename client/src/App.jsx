ae595d8e7efb7c904e3f234bd77c98ce74f82863
Gautam <gautamthakur0014@gmail.com>
2026-04-07 20:59:18 +0530

    frontend initialised

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+// import reactLogo from './assets/something.ext'
+
+function App() {
+  
+
+  return (
+    <>
+   <p>Hello</p>
+    </>
+  )
+}
+
+export default App
