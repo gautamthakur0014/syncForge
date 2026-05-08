5240bdce628586538244d3f0e2d19114bdcb9d21
Gautam <gautamthakur0014@gmail.com>
2026-05-08 23:56:14 +0530

    added toolbar

---
+++ client/src/components/toolbar/ToolBar.jsx
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+import ExecuteCode from "./ExecuteCode";
+import LanguageSelector from "./LanguageSelector";
+import RoomDropdown from "./RoomDropdown";
+import RoomMembers from "./RoomMembers";
+import ThemeSelector from "./ThemeSelector";
+
+const ToolBar = ({ fetchData }) => {
+    const roomIdRef = useRef(null);
+    const usernameRef = useRef(null);
+     const [theme, setTheme] = useState("light");
+
+     const handleSubmit = (e) => {
+       e.preventDefault();
+
+       const data = {
+         theme,
+         roomId: roomIdRef.current.value,
+         username: usernameRef.current.value,
+       };
+
+       console.log(data);
+     };
+
+
+  return (
+    <div className="flex justify-evenly items-center">
+      <ExecuteCode/>
+      <LanguageSelector/>
+      <RoomDropdown/>
+      <RoomMembers/>
+      <ThemeSelector/>
+    </div>
+  );
+};
+
+export default ToolBar;
+
+
