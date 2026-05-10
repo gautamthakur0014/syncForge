6cc97f2c45bddace63fb872c5879b49c25fa1fb7
Gautam <gautamthakur0014@gmail.com>
2026-05-10 23:59:53 +0530

    theme selector

---
+++ client/src/components/toolbar/ThemeSelector.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import useEditorStore from '../../store/useEditorStore';
+
+const ThemeSelector = () => {
+  const theme = useEditorStore((state)=>state.theme);
+  const setTheme = useEditorStore((state)=>state.setTheme)
+  return (
+    <div>
+      <label htmlFor="theme"></label>
+      <select
+        id="theme"
+        value={theme}
+        onChange={(e) => setTheme(e.target.value)}
+      >
+        <option value="vs">light</option>
+        <option value="vs-dark">dark</option>
+        <option value="hc-black">High Contrast Dark</option>
+        <option value="hc-light">High Contrast Light</option>
+      </select>
+    </div>
+  );
+}
+
+export default ThemeSelector
