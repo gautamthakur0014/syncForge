fb11ab56fa625e17f252c52cbc216fb255b9bc15
Gautam <gautamthakur0014@gmail.com>
2026-05-11 22:03:49 +0530

    Language Selector

---
+++ client/src/components/toolbar/LanguageSelector.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import useEditorStore from '../../store/useEditorStore';
+
+const LanguageSelector = () => {
+  const language = useEditorStore((state)=>state.language);
+  const setLanguage = useEditorStore((state)=>state.setLanguage);
+  return (
+    <div>
+      <label htmlFor="language"></label>
+      <select
+        id="language"
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+      >
+        <option value="javaScript">JavaScript </option>
+        {/* <option value="Python">Python</option>
+        <option value="C++">C++</option> */}
+      </select>
+    </div>
+  );
+}
+
+export default LanguageSelector
