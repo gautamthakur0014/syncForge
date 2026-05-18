c75355e4eaa2137bcf932befa16a577e4f521a8d
Gautam <gautamthakur0014@gmail.com>
2026-05-18 22:56:17 +0530

    created seperate files

--- client/src/pages/Playground.jsx
+++ client/src/pages/Playground.jsx
@@ -1,35 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MonacoEditor from "../components/editor/MonacoEditor";
-import axios from "axios";
 import Console from "../components/Console";
+import { getSocket } from "../utils/socket";
+import ToolBar from "../components/toolbar/ToolBar";
 
 const Playground = () => {
-  const [code, setCode] = useState("//start coding from here");
-  const [output, setOutput] =useState(null);
-  const fetchData = async () => {
-    try {
-      const res = await axios({
-        method: "post",
-        url: "http://localhost:5000/api/v1/execute",
-        data: {
-          language: "javascript",
-          code,
-        },
-      });
-      setOutput(res.data);
-    } catch (err) {
-      console.log(err);
-      setOutput(err.response?.data?.error || err.message);
-    }
-  };
+  const roomId = 1234;
+  const userName = "alex";
+  
+
 
   return (
     <div className="p-1 m-0 h-screen">
-      <MonacoEditor code={code} setCode={setCode} />
-      {/* <button className="bg-black text-2xl text-red-700" onClick={fetchData}>
-        run
-      </button> */}
-      <Console output={output}/>
+      <ToolBar/>
+      <MonacoEditor/>
+      <Console/>
     </div>
   );
 };
