d84cf7f8adce4b9a19905d1e70671cba1ef0c41a
Gautam <gautamthakur0014@gmail.com>
2026-05-30 21:46:20 +0530

    added actions:user

--- client/src/store/useRoomStore.js
+++ client/src/store/useRoomStore.js
@@ -1,16 +1,48 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 const useRoomStore = create(
   devtools(
-    (set) => ({
-      roomId: "",
-      users: [],
+    persist(
+      (set, get) => ({
+        userName: null,
+        roomId: null,
+        isInRoom: false,
+        users: [],
 
-      setRoomId: (id) => set({ roomId: id }),
+        actions: {
+          setRoomId: (id) => set({ roomId: id, isInRoom: true }),
+          leaveRoom: () =>
+            set({
+              roomId: null,
+              isInRoom: false,
+              users: [],
+            }),
 
-      setUsers: (usersList) => set({ users: usersList }),
-    }),
+          setUsers: (usersList) => set({ users: usersList }),
+          addUser: (username) =>
+            set((state) => ({
+              users: [...state.users, username],
+            })),
+
+          removeUser: (username) =>
+            set((state) => ({
+              users: state.users.filter((u) => u !== username),
+            })),
+
+          clearUsers: () => set({ users: [] }),
+        },
+      }),
+      {
+        name: "room-storage",
+
+        partialize: (state) => ({
+          roomId: state.roomId,
+          userName: state.userName,
+          isInRoom: state.isInRoom,
+        }),
+      },
+    ),
 
     {
       name: "RoomStore",
