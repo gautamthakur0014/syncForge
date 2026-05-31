f5d90aadbd407bd74ea81dcac493ed63e4d6f8d8
Gautam <gautamthakur0014@gmail.com>
2026-05-31 22:55:45 +0530

    added handle userJoined and left

--- client/src/hooks/socket/useRoomSocket.js
+++ client/src/hooks/socket/useRoomSocket.js
@@ -1,12 +1,30 @@
 import { useEffect } from "react";
 import { getSocket } from "../../utils/socket";
+import useRoomStore from "../../store/useRoomStore";
 
 export const useRoomSocket = () => {
-  const socket = getSocket();
+  const { actions, userName, roomId, isInRoom, users } = useRoomStore();
+
   useEffect(() => {
+    const socket = getSocket();
+
+    const handleRoomMembers = ({ members }) => {
+      console.log(members + "members");
+      actions.setUsers(members || []);
+    };
+
+    const handleUserJoined = ({ joinedUserName }) => {
+      console.log(joinedUserName + "joined");
+      actions.addUser(joinedUserName);
+    };
+    const handleUserLeft = ({ leavedUserName }) => {
+      console.log(leavedUserName);
+      actions.removeUser(leavedUserName);
+    };
+
     if (!roomId) return;
 
-    socket.emit("joinRoom", {roomId, userName});
+    // socket.emit("joinRoom", { roomId, userName });
     socket.on("roomMembers", handleRoomMembers);
     socket.on("userJoined", handleUserJoined);
     socket.on("userLeft", handleUserLeft);
