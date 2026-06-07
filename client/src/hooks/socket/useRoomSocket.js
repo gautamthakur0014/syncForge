a11dc9f1ea0aa6229ddde0f24c691533a79e9fbb
Gautam <gautamthakur0014@gmail.com>
2026-06-07 23:25:09 +0530

    fixed user left

--- client/src/hooks/socket/useRoomSocket.js
+++ client/src/hooks/socket/useRoomSocket.js
@@ -1,24 +1,33 @@
 import { useEffect } from "react";
 import { getSocket } from "../../utils/socket";
 import useRoomStore from "../../store/useRoomStore";
+import useEditorStore from "../../store/useEditorStore";
 
 export const useRoomSocket = () => {
   const { actions, userName, roomId, isInRoom, users } = useRoomStore();
+  const editorStore = useEditorStore();
 
   useEffect(() => {
     const socket = getSocket();
 
-    const handleRoomMembers = ({ members }) => {
-      console.log(members + "members");
+    const handleRoomMembers = ( {members, roomState} ) => {
+      // console.log(members.map((e)=>(e.userName)));
+      console.log(members);
+      
+      console.log(roomState);
+      editorStore.actions.setCode(roomState.code);
+      editorStore.actions.setLanguage(roomState.language);
+      editorStore.actions.setTheme(roomState.theme);
+      
       actions.setUsers(members || []);
     };
 
-    const handleUserJoined = ({ joinedUserName }) => {
-      console.log(joinedUserName + "joined");
-      actions.addUser(joinedUserName);
+    const handleUserJoined = ({ joinedUser }) => {
+      console.log(joinedUser.userName + " joined");
+      actions.addUser(joinedUser);
     };
     const handleUserLeft = ({ leavedUserName }) => {
-      console.log(leavedUserName);
+      console.log(leavedUserName + "leaved");
       actions.removeUser(leavedUserName);
     };
 
@@ -28,11 +37,15 @@ export const useRoomSocket = () => {
     socket.on("roomMembers", handleRoomMembers);
     socket.on("userJoined", handleUserJoined);
     socket.on("userLeft", handleUserLeft);
+    socket.on("roomError", ({ message }) => {
+      alert(message);
+    });
 
     return () => {
       socket.off("roomMembers");
       socket.off("userJoined");
       socket.off("userLeft");
+      socket.off("roomError");
     };
   }, [roomId]);
 };
