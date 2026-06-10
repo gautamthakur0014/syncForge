5549f243c95716c90501873e86c30cf5408bb5be
Gautam <gautamthakur0014@gmail.com>
2026-06-10 11:49:35 +0530

    fixed realtime roomState sync

--- server/src/sockets/codeSync.js
+++ server/src/sockets/codeSync.js
@@ -1,7 +1,9 @@
+const { updateRoomState } = require("../managers/roomManager");
+
 const codeSyncHandler = (io, socket) => {
   socket.on("sendCodeChange", ({ roomId, code }) => {
-    console.log("code");
-    console.log(code);
+    if(!roomId)return;
+    updateRoomState(roomId,code);
     
     socket.to(roomId).emit("receiveCodeChange", { code });
   });
