138fac456bb3aee8d78970b76ca91ca520656b8b
Gautam <gautamthakur0014@gmail.com>
2026-06-16 23:46:31 +0530

    added cursor sync

---
+++ client/src/utils/monaco/CursorLabelWidget.js
@@ -0,0 +1,43 @@
+import * as monaco from "monaco-editor";
+
+export default class CursorLabelWidget {
+  constructor(editor, userName, color, position) {
+    this.editor = editor;
+
+    this.id = `cursor-label-${userName}`;
+
+    this.position = position;
+
+    this.domNode = document.createElement("div");
+
+    this.domNode.className = "remote-cursor-label";
+
+    this.domNode.textContent = userName;
+
+    this.domNode.style.background = color;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getDomNode() {
+      console.log("getDomNode");
+
+    return this.domNode;
+  }
+
+  getPosition() {
+      console.log(this.position);
+
+    return {
+      position: this.position,
+
+      preference: [monaco.editor.ContentWidgetPositionPreference.ABOVE],
+    };
+  }
+
+  update(position) {
+    this.position = position;
+  }
+}
