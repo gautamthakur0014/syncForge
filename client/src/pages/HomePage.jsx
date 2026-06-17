168de028eb56ec78ee5904ef3404f1fa69dcc815
Gautam <gautamthakur0014@gmail.com>
2026-06-17 15:01:50 +0530

    homePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
-import {Link} from "react-router"
+import { Link } from "react-router";
 
 const HomePage = () => {
   return (
     <div>
-      this is home page
-      <Link to={"/Playground"} >
-      IDE
-      </Link>
+      <p>this is home page</p>
+      <div>
+        <button className="bg-blue-500 h-12 w-28 m-4 rounded-xl">
+          <Link to={"/Playground"}>Go to IDE</Link>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
