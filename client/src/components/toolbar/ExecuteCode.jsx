4d4068ff6f39d78622b1e808c19e47b626e8ec02
Gautam <gautamthakur0014@gmail.com>
2026-06-17 14:58:23 +0530

    execute code

--- client/src/components/toolbar/ExecuteCode.jsx
+++ client/src/components/toolbar/ExecuteCode.jsx
@@ -3,38 +3,12 @@ import useEditorStore from "../../store/useEditorStore";
 import axios from "axios";
 
 const ExecuteCode = () => {
-  const code = useEditorStore((state) => state.code);
-  const language = useEditorStore((state) => state.language);
 
-  const setOutput = useEditorStore((state) => state.setOutput);
-  const setIsRunning = useEditorStore((state) => state.setIsRunning);
-  const isRunning = useEditorStore((state)=>state.isRunning);
-
-  const runCode = async () => {
-    
-    try {
-      setIsRunning(true);
-
-    const res = await axios({
-      method: "post",
-      url: "http://localhost:5000/api/v1/execute",
-      data: {
-        language,
-        code,
-      },
-    });
-console.log(res);
-
-      setOutput(res.data);
-    } catch (err) {
-      setOutput(err);
-    } finally {
-      setIsRunning(false);
-    }
-  };
+  const{actions} = useEditorStore();
+  const isRunning = useEditorStore((state)=> state.isRunning);
 
   return (
-    <button className="bg-black text-2xl text-red-700" onClick={runCode}>
+    <button className="bg-black text-2xl text-red-700" onClick={actions.runCode}>
       {isRunning?"running":"run"}
     </button>
   );
