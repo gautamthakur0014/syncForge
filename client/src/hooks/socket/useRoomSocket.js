907cf65e589ba9c6c50425e4a3939f12f56bc591
Gautam <gautamthakur0014@gmail.com>
2026-06-28 22:16:59 +0530

    added joinroom event

--- client/src/hooks/socket/useRoomSocket.js
+++ client/src/hooks/socket/useRoomSocket.js
@@ -2,21 +2,46 @@ import { useEffect } from "react";
 import { getSocket } from "../../utils/socket";
 import useRoomStore from "../../store/useRoomStore";
 import useEditorStore from "../../store/useEditorStore";
+import * as Y from "yjs";
+import useYjsStore from "../../store/useYjsStore";
 
 export const useRoomSocket = () => {
   const { actions, userName, roomId, isInRoom, users } = useRoomStore();
   const editorStore = useEditorStore();
+  const ydoc = useYjsStore((s) => s.ydoc);
+  const language = useEditorStore((s) => s.language);
+  const theme = useEditorStore((s) => s.theme);
+  const useName = useRoomStore((s) => s.userName);
 
   useEffect(() => {
     const socket = getSocket();
+    if (!roomId || !ydoc) return;
+     const stateVector = Array.from(Y.encodeStateVector(ydoc));
+    socket.emit("joinRoom", {
+      roomId,
+      userName,
+      state: { language, theme },
+      ydocStateVector: stateVector,
+    });
 
-    const handleRoomMembers = ( {members, roomState} ) => {
+    const handleRoomMembers = ({ members, roomState, ydocState }) => {
+      if (!ydoc) return;
+
       // console.log(members.map((e)=>(e.userName)));
-      
-      editorStore.actions.setCode(roomState.code);
+
+      // editorStore.actions.setCode(roomState.code);
+      // console.log("roomMembers");
+      // console.log("ydoc exists", !!ydoc);
+      // console.log("binding yText", ydoc.getText("editor").toString());
+      // console.log(ydocState);
+
+      if (ydocState) {
+        Y.applyUpdate(ydoc, new Uint8Array(ydocState), "origin");
+      }
+      // console.log("after apply", ydoc.getText("editor").toString());
       editorStore.actions.setLanguage(roomState.language);
       editorStore.actions.setTheme(roomState.theme);
-      
+
       actions.setUsers(members || []);
     };
 
@@ -45,5 +70,5 @@ export const useRoomSocket = () => {
       socket.off("userLeft");
       socket.off("roomError");
     };
-  }, [roomId]);
+  }, [roomId, ydoc]);
 };
