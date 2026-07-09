74b5e910b1304d1a3b412f19c3a14bf846689a93
Gautam <gautamthakur0014@gmail.com>
2026-07-09 07:17:45 +0530

    removed logs

--- client/src/utils/monaco/CursorLabelWidget.js
+++ client/src/utils/monaco/CursorLabelWidget.js
@@ -18,19 +18,15 @@ export default class CursorLabelWidget {
   }
 
   getId() {
-    console.log("getId");
     return this.id;
   }
 
   getDomNode() {
-    console.log("getDomNode");
 
     return this.domNode;
   }
 
   getPosition() {
-    console.log("this.position");
-    console.log(this.position);
 
     return {
       position: this.position,
