85205d5cca264bf88e77ed036ec431710de1b00c
Gautam <gautamthakur0014@gmail.com>
2026-08-25 07:32:43 +0530

    add starter template

---
+++ client/src/utils/starterTemplate.js
@@ -0,0 +1,29 @@
+// starterTemplates.js
+
+export const starterTemplates = {
+  javascript: `console.log("Hello, World!");`,
+
+  python: `print("Hello, World!")`,
+
+  cpp: `#include <iostream>
+using namespace std;
+
+int main() {
+
+// write your code here
+
+    cout << "Hello, World!" << endl;
+
+
+    return 0;
+}`,
+
+  java: `public class Main {
+    public static void main(String[] args) {
+
+    // write your code here
+
+        System.out.println("Hello, World!");
+    }
+}`,
+};
--- client/src/yjs/createYDoc.js
+++ client/src/yjs/createYDoc.js
@@ -2,12 +2,18 @@
 
 import * as Y from "yjs";
 import useEditorStore from "../store/useEditorStore";
+import { starterTemplates } from "../utils/starterTemplate";
 
 export const createYDoc = () => {
+  const language = useEditorStore.getState().language;
   const ydoc = new Y.Doc();
   const yText = ydoc.getText("editor");
+
+  // Default language is JavaScript
+  if (yText.length === 0) {
+    yText.insert(0, starterTemplates[language]);
+  }
   yText.observe(() => {
-    
     useEditorStore.getState().actions.setCode(yText.toString());
   });
 
